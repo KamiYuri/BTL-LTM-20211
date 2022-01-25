ca6ec22a2b5c0b3bdeb06e86b3896d37f4358735
KamiYuri <dung.hv194254@sis.hust.edu.vn>
2022-01-25 20:52:49 +0700

    add login view

--- client/src/main/java/com/ltm/client/Controller/LoginController.java
+++ client/src/main/java/com/ltm/client/Controller/LoginController.java
@@ -1,4 +1,19 @@
 package com.ltm.client.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    TextField us_input, pw_input;
+
+    @FXML
+    public void submit(ActionEvent e){
+        String username, password;
+        username = us_input.getText();
+        password = pw_input.getText();
+
+        System.out.println(username + ":" + password);
+    }
 }
--- client/src/main/java/com/ltm/client/Main.java
+++ client/src/main/java/com/ltm/client/Main.java
@@ -1,20 +1,32 @@
 package com.ltm.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage){
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/login-view.fxml")));
+            Scene scene = new Scene(root);
+
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.setFullScreen(false);
+            stage.setResizable(false);
+
+            stage.show();
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
     }
 
     public static void main(String[] args) {
